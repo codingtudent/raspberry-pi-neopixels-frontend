a91aaf81f18444fd333b00478acf6a17e5b92c7e
mit <mitpatel6263@gmail.com>
2021-09-19 13:24:57 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿
+namespace raspberry_pi_neopixels_frontend
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.Login_page = new System.Windows.Forms.ToolStripMenuItem();
+            this.Control_page = new System.Windows.Forms.ToolStripMenuItem();
+            this.red = new System.Windows.Forms.Button();
+            this.green = new System.Windows.Forms.Button();
+            this.blue = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.clear = new System.Windows.Forms.Button();
+            this.rainbow = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(285, 203);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(169, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.richTextBox1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(10, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(778, 416);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(282, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "host ip";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(285, 95);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(169, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(6, 299);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(764, 96);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(282, 187);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "password";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(281, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "username";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(284, 147);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(169, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(335, 245);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(348, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Login";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Login_page,
+            this.Control_page});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            this.menuStrip1.Click += new System.EventHandler(this.MenuStrip1_Click);
+            // 
+            // Login_page
+            // 
+            this.Login_page.Name = "Login_page";
+            this.Login_page.Size = new System.Drawing.Size(49, 20);
+            this.Login_page.Text = "Login";
+            // 
+            // Control_page
+            // 
+            this.Control_page.Name = "Control_page";
+            this.Control_page.Size = new System.Drawing.Size(59, 20);
+            this.Control_page.Text = "Control";
+            // 
+            // red
+            // 
+            this.red.BackColor = System.Drawing.Color.Red;
+            this.red.Location = new System.Drawing.Point(81, 90);
+            this.red.Name = "red";
+            this.red.Size = new System.Drawing.Size(75, 20);
+            this.red.TabIndex = 4;
+            this.red.Text = "red";
+            this.red.UseVisualStyleBackColor = false;
+            this.red.Click += new System.EventHandler(this.Red_Click);
+            // 
+            // green
+            // 
+            this.green.BackColor = System.Drawing.Color.Green;
+            this.green.Location = new System.Drawing.Point(81, 116);
+            this.green.Name = "green";
+            this.green.Size = new System.Drawing.Size(75, 23);
+            this.green.TabIndex = 5;
+            this.green.Text = "green";
+            this.green.UseVisualStyleBackColor = false;
+            this.green.Click += new System.EventHandler(this.green_Click);
+            // 
+            // blue
+            // 
+            this.blue.BackColor = System.Drawing.Color.Blue;
+            this.blue.Location = new System.Drawing.Point(81, 145);
+            this.blue.Name = "blue";
+            this.blue.Size = new System.Drawing.Size(75, 23);
+            this.blue.TabIndex = 6;
+            this.blue.Text = "blue";
+            this.blue.UseVisualStyleBackColor = false;
+            this.blue.Click += new System.EventHandler(this.blue_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.clear);
+            this.groupBox2.Controls.Add(this.rainbow);
+            this.groupBox2.Controls.Add(this.red);
+            this.groupBox2.Controls.Add(this.blue);
+            this.groupBox2.Controls.Add(this.green);
+            this.groupBox2.Location = new System.Drawing.Point(280, 27);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(240, 280);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // clear
+            // 
+            this.clear.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.clear.Location = new System.Drawing.Point(81, 203);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(75, 23);
+            this.clear.TabIndex = 8;
+            this.clear.Text = "clear";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // rainbow
+            // 
+            this.rainbow.ForeColor = System.Drawing.Color.Crimson;
+            this.rainbow.Location = new System.Drawing.Point(81, 174);
+            this.rainbow.Name = "rainbow";
+            this.rainbow.Size = new System.Drawing.Size(75, 23);
+            this.rainbow.TabIndex = 7;
+            this.rainbow.Text = "rainbow";
+            this.rainbow.UseVisualStyleBackColor = true;
+            this.rainbow.Click += new System.EventHandler(this.rainbow_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem Login_page;
+        private System.Windows.Forms.ToolStripMenuItem Control_page;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button red;
+        private System.Windows.Forms.Button green;
+        private System.Windows.Forms.Button blue;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button rainbow;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Renci.SshNet;
+
+namespace raspberry_pi_neopixels_frontend
+{
+    public partial class Form1 : Form
+    {
+        //private delegate void SafeCallDelegate(string text);
+        public Form1()
+    {
+        InitializeComponent();
+        Control_page.Visible = false;
+    }
+
+    private void MenuStrip1_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+    {
+        if (e.ClickedItem.Text == "Login")
+        {
+            this.groupBox2.Visible = false;
+            this.groupBox1.Visible = true;
+        }
+        if (e.ClickedItem.Text == "Control")
+        {
+            this.groupBox2.Visible = true;
+            this.groupBox1.Visible = false;
+        }
+    }
+        string host;
+        string username;
+        string password;
+    private void button1_Click(object sender, EventArgs e)
+    {
+        richTextBox1.Clear();
+            //thread2 = new Thread(new ThreadStart(SetText));
+            //thread2.Start();
+            //Thread.Sleep(1000);
+
+            username = textBox2.Text;
+            password = textBox1.Text;
+            host = textBox3.Text;
+            richTextBox1.Text += string.Format("\n" + username);
+            richTextBox1.Text += string.Format("\n"+password);
+            richTextBox1.Text += string.Format("\n" + host);
+
+
+            //string remoteDirectory = "/home/pi";
+
+
+            using (SshClient sshclient = new SshClient(host, username, password))
+        {
+            try
+            {
+                sshclient.Connect();
+                Control_page.Visible = true;
+                /*
+                var files = sftp.ListDirectory(remoteDirectory);
+                Control_page.Visible = true;
+                foreach (var file in files)
+                {
+                    richTextBox1.Text += string.Format(file.Name);
+                }
+                sc.Execute();
+                sftp.Disconnect();
+                */
+
+                //SshCommand sc = sshclient.CreateCommand("sudo python3 rainbow.py");
+                //sc.Execute();
+                //richTextBox1.Text += string.Format(sc.Result);
+
+                //sc = sshclient.CreateCommand("1");
+                //sc.Execute();
+                sshclient.Disconnect();
+
+            }
+            catch (Exception er)
+            {
+                richTextBox1.Text += string.Format("An exception has been caught " + er.ToString());
+            }
+                
+        }
+    }
+
+
+    private void Red_Click(object sender, EventArgs e)
+    {
+        using (SshClient sshclient = new SshClient(host, username, password))
+        {
+            try
+            {
+                sshclient.Connect();
+                SshCommand sc = sshclient.CreateCommand("sudo python3 red.py");
+                sc.Execute();
+                richTextBox1.Text += string.Format(sc.Result);
+                //sshclient.Disconnect();
+            }
+            catch (Exception er)
+            {
+                richTextBox1.Text += string.Format("An exception has been caught " + er.ToString());
+            }
+        }
+    }
+
+        private void green_Click(object sender, EventArgs e)
+        {
+            using (SshClient sshclient = new SshClient(host, username, password))
+            {
+                try
+                {
+                    sshclient.Connect();
+                    SshCommand sc = sshclient.CreateCommand("sudo python3 green.py");
+                    sc.Execute();
+                    richTextBox1.Text += string.Format(sc.Result);
+                    sshclient.Disconnect();
+                }
+                catch (Exception er)
+                {
+                    richTextBox1.Text += string.Format("An exception has been caught " + er.ToString());
+                }
+            }
+        }
+
+        private void blue_Click(object sender, EventArgs e)
+        {
+            using (SshClient sshclient = new SshClient(host, username, password))
+            {
+                try
+                {
+                    sshclient.Connect();
+                    SshCommand sc = sshclient.CreateCommand("sudo python3 blue.py");
+                    sc.Execute();
+                    richTextBox1.Text += string.Format(sc.Result);
+                    sshclient.Disconnect();
+                }
+                catch (Exception er)
+                {
+                    richTextBox1.Text += string.Format("An exception has been caught " + er.ToString());
+                }
+            }
+        }
+
+        private void rainbow_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            using (SshClient sshclient = new SshClient(host, username, password))
+            {
+                try
+                {
+                    sshclient.Connect();
+                    SshCommand sc = sshclient.CreateCommand("sudo python3 blank.py");
+                    sc.Execute();
+                    richTextBox1.Text += string.Format(sc.Result);
+                    sshclient.Disconnect();
+                }
+                catch (Exception er)
+                {
+                    richTextBox1.Text += string.Format("An exception has been caught " + er.ToString());
+                }
+            }
+        }
+        /*
+private void WriteTextSafe(string text)
+{
+if (textBox1.InvokeRequired)
+{
+var d = new SafeCallDelegate(WriteTextSafe);
+textBox1.Invoke(d, new object[] { text });
+}
+else
+{
+textBox1.Text = text;
+}
+}
+*/
+        /*
+        private void SetText(string text)
+        {
+            Thread myThread = new System.Threading.Thread(delegate () {
+                string remoteDirectory = "/home/pi";
+
+                string host = @"192.168.1.24";
+                string username = "pi";
+                string password = @"raspbian";
+
+                using (SftpClient sftp = new SftpClient(host, username, password))
+                {
+                    try
+                    {
+                        sftp.Connect();
+
+                        var files = sftp.ListDirectory(remoteDirectory);
+
+                        foreach (var file in files)
+                        {
+                            richTextBox1.Text += string.Format(file.Name);
+                        }
+
+                        sftp.Disconnect();
+                    }
+                    catch (Exception er)
+                    {
+                        richTextBox1.Text += string.Format("An exception has been caught " + er.ToString());
+                    }
+                }
+            });
+
+            myThread.Start();
+            WriteTextSafe(text);
+        }
+        */
+    }
+
+}
